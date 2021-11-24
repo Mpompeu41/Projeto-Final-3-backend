ca05a495cf9918f7d18567c409469ee64cac5807
Mpompeu41 <89050561+Mpompeu41@users.noreply.github.com>
2021-11-23 23:32:44 -0300

    Add files via upload

---
+++ conn/conn.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Conn = (url, user, pass, data) => {
+  mongoose.connect(`${url}/${data}`, {
+      user: user,
+      pass: pass,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  }
+).then(()=>{
+  console.log('conexão executada com sucesso');
+}).catch((err)=>{
+  console.error(err);
+})
+}
+
+module.exports = Conn;
